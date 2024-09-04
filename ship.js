1d822ca3799e4ab7c7cd34c966b08cbe1b188ad8
maxx006 <maxf.20165@obhs.school.nz>
2024-09-04 12:29:33 +1200

    Add files via upload

--- ship.js
+++ ship.js
@@ -3,19 +3,35 @@
 function charterBooking(){
 	var totalCost = 0;
 	var extrasCost = 0;
-	alert("charter booking function starting")
 	var checkInDate = document.getElementById("checkInDate").value;
-	alert(checkInDate);
 	var guestAmount = document.getElementById("guestAmount").value;
-	alert(cruiseDuration);
 	var bookingPurpose =document.getElementById("bookingPurpose").value;
 	var cruiseDuration = document.getElementById("cruiseDuration").value;
 	var cruiseType =this.dataset.value;
 	var boatCost =this.dataset.price;
-	alert(cruiseType + cruiseDuration);
 	totalCost += Number(cruiseDuration*boatCost+extrasCost);
 	extraOptions = [];
 	var myForm = document.getElementsByClassName("extrasCheckbox")
+	
+	if(document.getElementById("checkInDate").validity.valueMissing){
+		alert("date not selected");
+		document.getElementById("checkInDate").scrollIntoView();
+		//add an inline error message here 
+		return;
+	}
+	
+	if (document.getElementById("cruiseDuration").validity.rangeUnderflow ||
+		document.getElementById("cruiseDuration").validity.rangeOverflow ||
+		document.getElementById ("cruiseDuration").validity.valueMissing) {
+			alert("Select duration of cruise");
+			document.getElementById("cruiseDuration").scrollIntoView();
+			//add in inline message here 
+			return;
+	}
+	
+	
+	
+	
 	for (i = 0; i < myForm.length; i++) {
 		if (myForm[i].type == "checkbox") {
 			if (myForm[i].checked) {
@@ -33,6 +49,20 @@ function charterBooking(){
 }
 
 
+function outPutSummary(checkInDate, cruiseDuration, guestAmount, cruiseType, extrasCost, totalCost, bookingPurpose, extraOptions, boatCost) {
+	document.getElementById("outputDate").innerHTML = checkInDate;
+	document.getElementById("durationOutput").innerHTML = cruiseDuration;
+	document.getElementById("cruiseOutput").innerHTML = cruiseType;
+	document.getElementById("purposeOutput").innerHTML = bookingPurpose;
+	document.getElementById("guestOutput").innerHTML = guestAmount;
+	document.getElementById("extrasOutput").innerHTML = extrasCost;
+	document.getElementById("optionsOutput").innerHTML = extraOptions;
+	document.getElementById("totalOutput").innerHTML = totalCost;
+	
+}
+
+function checkDetails(){}
+
 function pushData(){
 	alert("Push data function");
 	var Airtable = require('airtable');
@@ -41,8 +71,8 @@ function pushData(){
 	}).base('appxt1ddAlNkHN4c7');
 	console.log("Creating a record....");
 	base('Cruise').create({
-		"First Name": "Max",
-		"Last Name": "Fleet"
+		"First Name": firstNameInput,
+		"Last Name": lastNameInput
 	}, {
 		typecast: true
 	}, function(err, record) {
@@ -59,17 +89,3 @@ for (var i = 0; i < tiles.length; i++) {
 	// If a tile is clicked, it calls the selectExercise function
 	tiles[i].addEventListener('click', charterBooking);
 }
-
-function outPutSummary(checkInDate, cruiseDuration, guestAmount, cruiseType, extrasCost, totalCost, bookingPurpose, extraOptions, boatCost) {
-	document.getElementById("outputDate").innerHTML = checkInDate;
-	document.getElementById("durationOutput").innerHTML = cruiseDuration;
-	document.getElementById("cruiseOutput").innerHTML = cruiseType;
-	document.getElementById("purposeOutput").innerHTML = bookingPurpose;
-	document.getElementById("guestOutput").innerHTML = guestAmount;
-	document.getElementById("extrasOutput").innerHTML = extrasCost;
-	document.getElementById("optionsOutput").innerHTML = extraOptions;
-	document.getElementById("totalOutput").innerHTML = totalCost;
-	
-}
-
-function checkDetails(){}
