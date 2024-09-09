c3e66a04775e3fb9616b3d95733760e4ab8a2791
maxx006 <maxf.20165@obhs.school.nz>
2024-09-10 09:49:39 +1200

    Add files via upload

--- ship.js
+++ ship.js
@@ -1,36 +1,28 @@
 // JavaScript Document
-
-function charterBooking(){
+function charterBooking() {
 	var totalCost = 0;
 	var extrasCost = 0;
 	var checkInDate = document.getElementById("checkInDate").value;
 	var guestAmount = document.getElementById("guestAmount").value;
-	var bookingPurpose =document.getElementById("bookingPurpose").value;
+	var bookingPurpose = document.getElementById("bookingPurpose").value;
 	var cruiseDuration = document.getElementById("cruiseDuration").value;
 	var cruiseType = document.getElementById("cruiseType").value;
-	var pricePerHour =this.dataset.price;
-	totalCost += Number(cruiseDuration*pricePerHour+extrasCost);
+	var pricePerHour = this.dataset.price;
+	totalCost += Number(cruiseDuration * pricePerHour + extrasCost);
 	extraOptions = [];
 	var myForm = document.getElementsByClassName("extrasCheckbox")
-	
-	if(document.getElementById("checkInDate").validity.valueMissing){
+	if (document.getElementById("checkInDate").validity.valueMissing) {
 		alert("date not selected");
 		document.getElementById("checkInDate").scrollIntoView();
 		//add an inline error message here 
 		return;
 	}
-	
-	if (document.getElementById("cruiseDuration").validity.rangeUnderflow ||
-		document.getElementById("cruiseDuration").validity.rangeOverflow ||
-		document.getElementById ("cruiseDuration").validity.valueMissing) {
-			alert("Select duration of cruise");
-			document.getElementById("cruiseDuration").scrollIntoView();
-			//add 
-			return;
+	if (document.getElementById("cruiseDuration").validity.rangeUnderflow || document.getElementById("cruiseDuration").validity.rangeOverflow || document.getElementById("cruiseDuration").validity.valueMissing) {
+		alert("Select duration of cruise");
+		document.getElementById("cruiseDuration").scrollIntoView();
+		//add 
+		return;
 	}
-	
-	
-	
 	for (i = 0; i < myForm.length; i++) {
 		if (myForm[i].type == "checkbox") {
 			if (myForm[i].checked) {
@@ -41,14 +33,13 @@ function charterBooking(){
 			}
 		}
 	}
-	totalCost = Number(cruiseDuration*pricePerHour + extrasCost);
+	totalCost = Number(cruiseDuration * pricePerHour + extrasCost);
 	alert("total cost" + totalCost);
 	document.getElementById("outputDate").scrollIntoView();
-	outPutSummary(checkInDate, cruiseDuration, guestAmount, cruiseType, extrasCost, totalCost, bookingPurpose,extraOptions,pricePerHour);
+	outPutSummary(checkInDate, cruiseDuration, guestAmount, cruiseType, extrasCost, totalCost, bookingPurpose, extraOptions, pricePerHour);
 }
 
-
-function outPutSummary(checkInDate, cruiseDuration, guestAmount, cruiseType, extrasCost, totalCost, bookingPurpose, extraOptions,pricePerHour) {
+function outPutSummary(checkInDate, cruiseDuration, guestAmount, cruiseType, extrasCost, totalCost, bookingPurpose, extraOptions, pricePerHour) {
 	document.getElementById("outputDate").innerHTML = checkInDate;
 	document.getElementById("durationOutput").innerHTML = cruiseDuration;
 	document.getElementById("cruiseOutput").innerHTML = cruiseType;
@@ -57,10 +48,10 @@ function outPutSummary(checkInDate, cruiseDuration, guestAmount, cruiseType, ext
 	document.getElementById("extrasOutput").innerHTML = "$" + extrasCost;
 	document.getElementById("optionsOutput").innerHTML = extraOptions;
 	document.getElementById("totalOutput").innerHTML = "$" + totalCost;
-    checkDetails(checkInDate, cruiseDuration, guestAmount, cruiseType, extrasCost, totalCost, bookingPurpose, extraOptions);	
+	checkDetails(checkInDate, cruiseDuration, guestAmount, cruiseType, extrasCost, totalCost, bookingPurpose, extraOptions);
 }
 
-function checkDetails(checkInDate,cruiseDuration,cruiseType,bookingPurpose,guestAmount,extrasCost,extraOptions,totalCost){
+function checkDetails(checkInDate, cruiseDuration, cruiseType, bookingPurpose, guestAmount, extrasCost, extraOptions, totalCost) {
 	alert("customer details function");
 	var firstname = firstNameInput.value;
 	var lastname = lastNameInput.value;
@@ -69,12 +60,11 @@ function checkDetails(checkInDate,cruiseDuration,cruiseType,bookingPurpose,guest
 	var driversLicence = driversLicenceInput.value;
 	var age = ageInput.value;
 	var comments = commentsInput.value;
-	alert(firstname + lastname + cellphone);
 	outputcheckDetails.innerHTML = firstname + " " + lastname + " " + cellphone + " " + email + " " + driversLicence + " " + age + " " + comments;
-	pushData(firstname, lastname, cellphone, email, driversLicence, age, comments,checkInDate,cruiseDuration,cruiseType,bookingPurpose,guestAmount,extrasCost,extraOptions,totalCost);
+	pushData(firstname, lastname, cellphone, email, driversLicence, age, comments, checkInDate, cruiseDuration, cruiseType, bookingPurpose, guestAmount, extrasCost, extraOptions, totalCost);
 }
 
-function pushData(firstname, lastname, cellphone, email, driversLicence, age, comments,checkInDate,cruiseDuration,cruiseType,bookingPurpose,guestAmount,extrasCost,extraOptions,totalCost){
+function pushData(firstname, lastname, cellphone, email, driversLicence, age, comments, checkInDate, cruiseDuration, cruiseType, bookingPurpose, guestAmount, extrasCost, extraOptions, totalCost) {
 	alert("Push data function");
 	var Airtable = require('airtable');
 	var base = new Airtable({
@@ -105,10 +95,29 @@ function pushData(firstname, lastname, cellphone, email, driversLicence, age, co
 		}
 		console.log("Record created: " + record.getId());
 	});
+	// When the user clicks the button, open the modal
+	alert("modal is about to pop up");
+	modal.style.display = "block";
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+ 	if (event.target == modal) {
+		modal.style.display = "none";
+ 		}
+	}
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+ 	modal.style.display = "none";
+	}
+	// Refresh the page after a delay of 3 seconds
+	setTimeout(function(){
+	location.reload();
+	}, 5000); // 5000 milliseconds = 5 seconds
+    console.log("End Function.");
 }
+
 // Add event listeners to exercise to room card by using a for loop
 var tiles = document.getElementsByClassName('card');
 for (var i = 0; i < tiles.length; i++) {
-	// If a tile is clicked, it calls the selectExercise function
-	tiles[i].addEventListener('click', charterBooking,outPutSummary,checkDetails,pushData);
+	// If a tile is clicked, it calls the outputSummary function
+	tiles[i].addEventListener('click', charterBooking, outPutSummary);
 }
